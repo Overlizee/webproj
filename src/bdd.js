ceb1b1d95aa6089789bf3fc2623decfb24add59a
Matteo91370 <73714489+Matteo91370@users.noreply.github.com>
2022-10-05 01:24:36 +0200

    Added database v1
    Database v1 to have a few examples to work on, but still to complete.

---
+++ src/bdd.js
@@ -0,0 +1,55 @@
+const info = [
+    {
+        date: '2022-11-20',
+        time: '16:00',
+        round: '1',
+        group: 'A',
+        country1: 'Qatar',
+        country1_img: 'https://lesplusbeauxdrapeauxdumonde.files.wordpress.com/2017/03/qatar-162396_1280.png?w=600&h=400',
+        country2: 'Ecuador',
+        country2_img: 'https://lesplusbeauxdrapeauxdumonde.files.wordpress.com/2016/09/ecuador-162283_1280.png?w=600&h=400',
+        stadium: 'Al Bayt Stadium',
+        capacity_stadium: '60000',
+        city_stadium: 'Al Khor'
+    },
+    {
+        date: '2022-11-21',
+        time: '13:00',
+        round: '1',
+        group: 'B',
+        country1: 'England',
+        country1_img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Flag_of_England.svg/800px-Flag_of_England.svg.png?20210524072131',
+        country2: 'Iran',
+        country2_img: 'https://lesplusbeauxdrapeauxdumonde.files.wordpress.com/2017/01/iran-26826_1280.png?w=600&h=400',
+        stadium: 'Khalifa International Stadium',
+        capacity_stadium: '50000',
+        city_stadium: 'Doha'
+    },
+    {
+        date: '2022-11-21',
+        time: '16:00',
+        round: '1',
+        group: 'A',
+        country1: 'Senegal',
+        country1_img: 'https://lesplusbeauxdrapeauxdumonde.files.wordpress.com/2017/06/senegal-26914_1280.png?w=600&h=400',
+        country2: 'Netherlands',
+        country2_img: 'https://lesplusbeauxdrapeauxdumonde.files.wordpress.com/2017/03/netherlands-26885_1280.png?w=600&h=400',
+        stadium: 'Al-Thumama Stadium',
+        capacity_stadium: '40000',
+        city_stadium: 'Al-Thumama'
+    },
+    {
+        date: '2022-11-21',
+        time: '19:00',
+        round: '1',
+        group: 'B',
+        country1: 'USA',
+        country1_img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png',
+        country2: 'Wales',
+        country2_img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Flag_of_Wales.svg/800px-Flag_of_Wales.svg.png?20220424105608',
+        stadium: 'Ahmed bin Ali Stadium',
+        capacity_stadium: '40000',
+        city_stadium: 'Al Rayyan'
+    },
+  ]
+  module.exports = info;
